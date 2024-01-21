25e293cc85bba095eaab6b8b5ba65f34cb8e97ca
Fakhreza SUparyadi <fakhreza.spy@gmail.com>
2024-01-21 13:24:55 +0700

    Setup Nest JS

---
+++ README/Installed.md
@@ -0,0 +1,5 @@
+npm i sequelize-typescript
+npm install --save sequelize
+npm i --save @nestjs/config  
+npm install sequelize reflect-metadata sequelize-typescript
+npm install --save-dev @types/node @types/validator
---
+++ src/config.ts
@@ -0,0 +1,26 @@
+import * as Joi from 'joi';
+
+export const configuration = () => ({
+    // Define your schema here
+    DB_HOST: process.env.DB_HOST || 'localhost',
+    DB_PORT: process.env.DB_PORT,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_NAME: process.env.DB_NAME,
+    // DB_SCHEMA: process.env.DB_SCHEMA || 'public',
+    // DB_MONGO_HISTORICAL_URI: process.env.DB_MONGO_HISTORICAL_URI,
+    // EVENTSTORE_READINESS_URL: process.env.EVENTSTORE_READINESS_URL,
+    // DIR_I18N: process.env.DIR_I18N || 'dist/i18n' 
+});
+
+export const configValidationSchema = Joi.object({
+    DB_HOST: Joi.string().required(),
+    DB_PORT: Joi.number().required(),
+    DB_USER: Joi.string().required(),
+    DB_PASSWORD: Joi.string(),
+    DB_NAME: Joi.string().required(),
+    // DB_SCHEMA: Joi.string().default('public'),
+    // DB_MONGO_HISTORICAL_URI: Joi.string().required(),
+    // EVENTSTORE_READINESS_URL: Joi.string().required(),
+    // DIR_I18N: Joi.string().default('dist/i18n')
+});
